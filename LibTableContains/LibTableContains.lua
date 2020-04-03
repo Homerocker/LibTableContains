e415d4b71f20b60c05ba2527c4e075bb834c0302
Homerocker <molodoy3561@gmail.com>
2020-04-03 17:23:28 +0300

    implemented LibStub

--- LibTableContains/LibTableContains.lua
+++ LibTableContains/LibTableContains.lua
@@ -1,5 +1,22 @@
-function table.contains(table, value)
+local MAJOR, MINOR = "LibTableContains", tonumber(GetAddOnMetadata("LibTableContains", "Version"):match("(%d+)"))
+local lib = LibStub:NewLibrary(MAJOR, MINOR)
+
+if not lib then return end
+
+function table.contains(table, value, ci, trim)
+  if ci then
+    value = string.lower(value)
+  end
+  if trim then
+    value = string.gsub(value, "%s+", "")
+  end
   for k, v in pairs(table) do
+    if ci then
+      v = string.lower(v)
+    end
+    if trim then
+      v = string.gsub(v, "%s+", "")
+    end
     if value == v then
       return k
     end
